c86af61f442bcc7bdb6b3cc1bf043a780aa36268
prasanna0913 <dspprasanna0913@gmail.com>
2021-02-02 13:44:09 -0500

    updated the legend to the map

--- Leaflet Step-1/static/js/logic.js
+++ Leaflet Step-1/static/js/logic.js
@@ -95,7 +95,29 @@ function createMap(earthquakes) {
         collapsed: false
     }).addTo(myMap);
 
+    var legend = L.control({ position: 'bottomright' });
+
+    legend.onAdd = function (map) {
+        var div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 30, 50, 70, 90],
+            labels = [];
+
+        div.innerHTML += 'Magnitude<br><hr>'
+
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }
+
+        return div;
+    };
+
+    legend.addTo(myMap);
 }
+
+
+
 
 
 
