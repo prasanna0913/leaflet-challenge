bf6df314d53002ffcbddb8dfaee402bd5bae8ff2
prasanna0913 <dspprasanna0913@gmail.com>
2021-02-02 13:32:37 -0500

    created the function

--- Leaflet Step-1/static/js/logic.js
+++ Leaflet Step-1/static/js/logic.js
@@ -5,12 +5,29 @@ d3.json(url, function (data) {
     console.log(data.features);
 
 });
+function getColor(d) {
+    console.log(d);
 
-// Perform a GET request to the query URL
-d3.json(url, function (data) {
-    // Once we get a response, send the data.features object to the createFeatures function
-    createFeatures(data.features);
-});
+    return (d >= -10 && d < 10) ? '#a3f621' :
+        (d >= 10 && d < 30) ? '#dcf425' :
+            (d >= 30 && d < 50) ? '#f6db25' :
+                (d >= 50 && d < 70) ? '#fdb72a' :
+                    (d >= 70 && d < 90) ? '#fca35d' :
+                        '#ff5f65';
+}
+
+
+function createFeatures(earthquakes) {
+    console.log(earthquakes);
+
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + 'Place:' + ' ' + feature.properties.place +
+            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
+            "</h3><hr><p>" + 'Magnitude:' + ' ' + feature.properties.mag + "</p>");
+    }
+
 
 
 
