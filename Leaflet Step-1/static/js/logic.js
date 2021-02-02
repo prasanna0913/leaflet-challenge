ad0a13442f89f5dd04f3195fa6e424885e9a5d99
prasanna0913 <dspprasanna0913@gmail.com>
2021-02-02 13:44:57 -0500

    added the legend to the map

--- Leaflet Step-1/static/js/logic.js
+++ Leaflet Step-1/static/js/logic.js
@@ -116,7 +116,11 @@ function createMap(earthquakes) {
     legend.addTo(myMap);
 }
 
-
+// Perform a GET request to the query URL
+d3.json(url, function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function
+    createFeatures(data.features);
+});
 
 
 
